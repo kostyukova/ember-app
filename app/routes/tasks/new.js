09765d7070736b07be960a23ee24d58d8c5acb5d
Elena Kostyukova <w.kostyukova@gmail.com>
2017-10-24 10:28:18 +0300

    new controllers

--- app/routes/tasks/new.js
+++ app/routes/tasks/new.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    addTask: function(){
+      var title = this.get('title');
+      var description = this.get('description');
+      var date = this.get('date');
+      alert(title);
+      // create new task
+    }
+  }
 });
